5cbaf5ede3e5653772e172e14712101e7d136ca1
mateolafalce <mateolafalce3@gmail.com>
2025-01-17 18:21:23 -0300

    String to &'static str AND add based distros

--- distrohoop/src/distros.rs
+++ distrohoop/src/distros.rs
@@ -1,8 +1,8 @@
 use colored::Color;
 
 pub struct Distro {
-    pub name: String,
-    pub description: String,
+    pub name: &'static str,
+    pub description: &'static str,
     pub color: colored::Color,
     pub is_bold: bool
 }
@@ -10,69 +10,81 @@ pub struct Distro {
 pub fn get_distros() -> Vec<Distro> {
     vec![
         Distro {
-            name: "Ubuntu".to_string(),
-            description: "Ubuntu is a popular Linux distribution based on Debian. It used to be really nice, but then they added snaps and a lot of bloat. Still a solid choice".to_string(),
+            name: "Ubuntu",
+            description: "Ubuntu is a popular Linux distribution based on Debian. It used to be really nice, but then they added snaps and a lot of bloat. Still a solid choice",
             color: Color::Red,
             is_bold: false,
         },
         Distro {
-            name: "Fedora".to_string(),
-            description: "Fedora is a cutting-edge Linux distribution sponsored by Red Hat. Solid choice, even Linus Torvalds uses it!".to_string(),
+            name: "Fedora",
+            description: "Fedora is a cutting-edge Linux distribution sponsored by Red Hat. Solid choice, even Linus Torvalds uses it!",
             color: Color::Blue,
             is_bold: true,
         },
         Distro {
-            name: "Debian".to_string(),
-            description: "Debian is a stable and versatile Linux distribution. Pretty much the holy grail of modern distros.".to_string(),
+            name: "Debian",
+            description: "Debian is a stable and versatile Linux distribution. Pretty much the holy grail of modern distros.",
             color: Color::Red,
             is_bold: false,
         },
         Distro {
-            name: "Arch Linux".to_string(),
-            description: "Arch Linux is a lightweight and flexible Linux distribution. Go ahead, say the words, you use Arch btw..".to_string(),
+            name: "Arch Linux",
+            description: "Arch Linux is a lightweight and flexible Linux distribution. Go ahead, say the words, you use Arch btw..",
             color: Color::Cyan,
             is_bold: true,
         },
         Distro {
-            name: "openSUSE".to_string(),
-            description: "openSUSE is a versatile and stable Linux distribution. And I like the little chameleon".to_string(),
+            name: "openSUSE",
+            description: "openSUSE is a versatile and stable Linux distribution. And I like the little chameleon",
             color: Color::BrightGreen,
             is_bold: true,
         },
         Distro {
-            name: "Linux Mint".to_string(),
-            description: "Linux Mint is a user-friendly Linux distribution based on Ubuntu. Really a solid choice if you want to get work done. Used this one a long time myself! :D".to_string(),
+            name: "Linux Mint",
+            description: "Linux Mint is a user-friendly Linux distribution based on Ubuntu. Really a solid choice if you want to get work done. Used this one a long time myself! :D",
             color: Color::Green,
             is_bold: false,
         },
         Distro {
-            name: "Gentoo".to_string(),
-            description: "Gentoo is a flexible and source-based Linux distribution. Have fun compling stuff for hours haha!".to_string(),
+            name: "Gentoo",
+            description: "Gentoo is a flexible and source-based Linux distribution. Have fun compling stuff for hours haha!",
             color: Color::Magenta,
             is_bold: true,
         },
         Distro {
-            name: "Void Linux".to_string(),
-            description: "Void Linux is an independent Linux distribution focused on simplicity. Extra points because this one sounds really cool".to_string(),
+            name: "Void Linux",
+            description: "Void Linux is an independent Linux distribution focused on simplicity. Extra points because this one sounds really cool",
             color: Color::Green,
             is_bold: true,
         },
         Distro {
-            name: "EndeavourOS".to_string(),
-            description: "EndeavourOS is Arch but without all the pain. Really nice choice! :)".to_string(),
+            name: "EndeavourOS",
+            description: "EndeavourOS is Arch but without all the pain. Really nice choice! :)",
             color: Color::Magenta,
             is_bold: true,
         },
         Distro {
-            name: "NixOS".to_string(),
-            description: "NixOS is a Linux distribution based on Nix package manager. It's really cool if you like to manage your system with code and stuff".to_string(),
+            name: "NixOS",
+            description: "NixOS is a Linux distribution based on Nix package manager. It's really cool if you like to manage your system with code and stuff",
             color: Color::BrightMagenta,
             is_bold: true,
         },
         Distro {
-            name: "Linux From Scratch".to_string(),
-            description: "Linux From Scratch huh? So you have chosen death. Just kidding, this one is really cool if you want to learn how Linux works from the ground up".to_string(),
+            name: "Linux From Scratch",
+            description: "Linux From Scratch huh? So you have chosen death. Just kidding, this one is really cool if you want to learn how Linux works from the ground up",
             color: Color::BrightRed,
+            is_bold: false,
+        },
+        Distro {
+            name: "OpenBSD",
+            description: "OpenBSD is a Unix-like operating system focused on security, simplicity, and code correctness. It's a top choice for secure servers and firewalls.",
+            color: Color::Yellow,
+            is_bold: true,
+        },
+        Distro {
+            name: "FreeBSD",
+            description: "FreeBSD is a Unix-like operating system known for its performance and advanced networking capabilities. It's great for servers, desktops, and embedded systems.",
+            color: Color::White,
             is_bold: false,
         },
     ]
