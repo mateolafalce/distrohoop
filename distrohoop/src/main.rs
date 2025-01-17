1dd1494662be8a42f9ede4d1f5d2f816766ce907
mateolafalce <mateolafalce3@gmail.com>
2025-01-17 18:46:31 -0300

    crossterm

--- distrohoop/src/main.rs
+++ distrohoop/src/main.rs
@@ -1,8 +1,23 @@
+use crossterm::{
+    cursor,
+    execute,
+    terminal::{self, ClearType},
+    ExecutableCommand,
+};
+use std::io::{stdout, Write};
 
 mod distros;
 mod utils;
 
-
-fn main() {
+fn main() -> std::io::Result<()> {
+    let mut stdout = stdout();
+    let original_mode = terminal::enable_raw_mode()?;
+    stdout.execute(terminal::EnterAlternateScreen)?;
+    stdout.execute(cursor::Hide)?;
+    stdout.flush()?;
     utils::play_animation();
+    stdout.execute(terminal::LeaveAlternateScreen)?;
+    stdout.execute(cursor::Show)?; 
+    terminal::disable_raw_mode()?;
+    Ok(())
 }
